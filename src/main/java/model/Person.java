b2f10aecaba0c4948887e4f5556fd9973c558bda
Thanh Dat <tthanhdat.pb@gmail.com>
2024-03-11 18:19:48 +0700

    update

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,5 +1,10 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Person {
 
     private String firstName;
@@ -11,22 +16,6 @@ public class Person {
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
